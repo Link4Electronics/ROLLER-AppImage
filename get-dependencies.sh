366e05cf42e55c8500de3c397444ac9f0fb3a1dd
Link4Electronics <link4electronics@gmail.com>
2026-08-04 19:38:37 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,7 +19,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package
+make-aur-package adlmidi
 
 # If the application needs to be manually built that has to be done down here
 echo "Making nightly build of ROLLER..."
