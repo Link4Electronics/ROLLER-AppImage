126158831b729daa353a983bd1bd586e698fcbff
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:58:52 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,8 +21,7 @@ get-debloated-pkgs --add-common --prefer-nano
 # Comment this out if you need an AUR package
 make-aur-package adlmidi
 
-# If the application needs to be manually built that has to be done down here
-echo "Making nightly build of ROLLER..."
+echo "Building ROLLER..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/FatalDecomp/ROLLER"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
