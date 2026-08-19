46dafb788a295c3ecd119d384dc21b2262f59843
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:59:01 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,5 @@ export DEPLOY_OPENGL=1
 quick-sharun ./AppDir/bin/roller /usr/lib/libWildMidi.so*
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
