bf1156f3efaee6ba552aae6b5c2b819d3726d4a2
Link4Electronics <link4electronics@gmail.com>
2026-08-23 19:28:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,6 @@ echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     cmake          \
     libcdio        \
-    libdecor       \
     sdl3           \
     sdl3_image     \
     shaderc        \
@@ -18,9 +17,8 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
 make-aur-package adlmidi
 
 echo "Building ROLLER..."
